f8b343ab93b837c22fa22e79f67b1252080ca88b
amrutha-ranjakumar <147486960+amrutha-ranjakumar@users.noreply.github.com>
2024-03-02 12:43:25 +0530

    portfolio react project

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,7 +23,7 @@ function About() {
           </Col>
           <Col style={{ marginRight: "60px" }}>
             <div className='d-flex mb-5'>
-              <div style={{ width: "400%" }} >
+              <div style={{ width: "300%" }} >
                 <h3 className=' ms-4 mt-5  mb-4 me-5 text-warning  ' >I AM<span className='text-dark'>  AMRUTHA </span><span className='text-dark'>Ranjakumar</span></h3>
                 <p className='text-dark '>As a dedicated learner and aspiring MERN Stack Developer Intern, I am deeply committed to crafting innovative web solutions. I am a self-motivated individual with a strong passion for web development. My skillset includes proficiency in HTML, CSS, React, Bootstrap, JavaScript, and Git. I am eager to apply these skills to create impactful and dynamic web applications.</p>
                 <p className='text-dark mt-4'>....................................................................................................................................</p>
@@ -35,7 +35,7 @@ function About() {
       </div>
       
      
-      <button className='border border-warning ' style={{ width: "1200px", marginLeft: "20px", marginTop: "100px" }}></button>
+     
     </>
   )
 }
